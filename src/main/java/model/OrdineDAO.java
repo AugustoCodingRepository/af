b22bc1691a418368a315effcbe2c4385d14b7035
karab <karab@FreeKara.fritz.box>
2024-06-30 17:11:26 +0200

    Modifica OrdineDAO aggiunta di Cost

--- src/main/java/model/OrdineDAO.java
+++ src/main/java/model/OrdineDAO.java
@@ -9,19 +9,20 @@ import java.util.List;
 
 public class OrdineDAO {
 
-    public static void insert(Ordine ordine, User user) throws SQLException {
-        if (user != null) {
-            Connection connection = ConnectToDB.getConnection();
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO Ordine (Order_ID, User_ID, Order_Data, Delivery_Data) VALUES (?, ?, ?, ?)");
-            statement.setInt(1, ordine.getOrder_ID());
-            statement.setInt(2, ordine.getUser_ID());
-            statement.setDate(3, ordine.getOrder_Data());
-            statement.setDate(4, ordine.getDelivery_Data());
-            statement.executeUpdate();
-        } else {
-            throw new SQLException("Solo gli utenti registrati possono effettuare un ordine.");
-        }
-    }
+	public static void insert(Ordine ordine, User user) throws SQLException {
+	    if (user != null) {
+	        Connection connection = ConnectToDB.getConnection();
+	        PreparedStatement statement = connection.prepareStatement("INSERT INTO Ordine (Order_ID, User_ID, Order_Data, Delivery_Data, Cost) VALUES (?, ?, ?, ?, ?)");
+	        statement.setInt(1, ordine.getOrder_ID());
+	        statement.setInt(2, ordine.getUser_ID());
+	        statement.setDate(3, ordine.getOrder_Data());
+	        statement.setDate(4, ordine.getDelivery_Data());
+	        statement.setInt(5, ordine.getCost());
+	        statement.executeUpdate();
+	    } else {
+	        throw new SQLException("Solo gli utenti registrati possono effettuare un ordine.");
+	    }
+	}
 
     public static List<Ordine> getAll() throws SQLException {
         Connection connection = ConnectToDB.getConnection();
